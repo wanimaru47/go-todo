e55f1e1328ad74ebc6125f7aacb4d46fb5762677
wanimaru47 <wanimaru47@gmail.com>
2018-08-14 16:05:37 +0900

    check box

--- controller/task.go
+++ controller/task.go
@@ -91,5 +91,24 @@ func (t *Todo) Delete(w http.ResponseWriter, r *http.Request) error {
 }
 
 func (t *Todo) Toggle(w http.ResponseWriter, r *http.Request) error {
-	return JSON(w, http.StatusNotImplemented, nil)
+	var todo model.Todo
+	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
+		return err
+	}
+
+	if err := TXHandler(t.DB, func(tx *sqlx.Tx) error {
+		result, err := todo.Toggle(tx)
+		if err != nil {
+			return err
+		}
+		if err := tx.Commit(); err != nil {
+			return err
+		}
+		todo.ID, err = result.LastInsertId()
+		return err
+	}); err != nil {
+		return err
+	}
+
+	return JSON(w, http.StatusOK, todo)
 }
